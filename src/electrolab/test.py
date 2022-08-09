b8ed36de5118a6f4020ae20ee2c4eb001c86e3f9
Oliver Rodriguez <oliver.rdz@gmail.com>
2022-08-09 14:28:20 -0500

    Included changing of dispenser nozzle

--- src/electrolab/test.py
+++ src/electrolab/test.py
@@ -1,26 +1,24 @@
 import controller
-import numpy as np
 
 # General setup
 port = '/dev/ttyACM0'
 baud_rate = 115200
 controller.Setup(port, baud_rate)
 
-# Positioning the head
-move_head = controller.Move_head(11,32)
-move_head.run()
+##### Positioning the head
+controller.Position_in_cell(3).run()
 
-# Operation setup
-dispense = controller.Dispense(wait_time=[1,1,1,1])
-rinse = controller.Rinse(wait_time=[1,1,1,1,1,1,1])
-dry = controller.Dry(wait_time=[1,1,1])
+##### Operation setup
+dispense = controller.Dispense(nozzle=2,wait_time=[1,1,1,1])
+#rinse = controller.Rinse(wait_time=[1,1,1,1,1,1,1])
+#dry = controller.Dry(wait_time=[1,1,1])
 
-# Execution
+##### Execution
 dispense.run()
 #rinse.run()
 #dry.run()
-rinse.run()
+#rinse.run()
 
-move_head = controller.Move_head(32,00)
-move_head.run()
-
+##### Return head to home and dispensing nozzle to 1
+controller.Position_in_cell(0).run()
+controller.change_dispenser_nozzle(controller.nozzle_n,1)

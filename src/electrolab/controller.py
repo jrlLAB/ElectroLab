862aedd95ca5b9e27256e4f52590297dd1dbfbe4
Oliver Rodriguez <oliver.rdz@gmail.com>
2022-08-09 11:38:12 -0500

    Added movement between cells

--- src/electrolab/controller.py
+++ src/electrolab/controller.py
@@ -1,5 +1,6 @@
 import serial
 import time
+import numpy as np
 
 #__options__ = ['X_STEPPER', 'Y_STEPPER', 'Z_STEPPER', 'Z_CLEANER', 'Z_ARGON', 
 #               'Z_DISPENSER', 'PUMP1_STEPPER', 'PUMP2_STEPPER', 'PUMP3_STEPPER',
@@ -8,40 +9,101 @@ import time
 # Global variables
 port_ = 'XXX'
 baudRate = 123
-#global state
-state = 1
+state = 1 # Nozzle state (1: dispensing, 2: rinsing, 3: drying)
+position = 0 # Head position, 0: home, 1: cell 1, 2: cell2, etc
+move_speed = 1000 # Movement speed
+dx0 = 4400 # dx for the home position
+dx = 2000
+dy = 2800
 
 class Setup:
-    def __init__(self, port, baud_rate):
+    def __init__(self, port, baud_rate=baudRate, speed=move_speed):
         global port_
         port_ = port
         global baudRate
         baudRate = baud_rate
+        global move_speed
+        move_speed = speed
         print('----------')
         print('Port: ', port_)
         print('baud_rate: ', baudRate)
+        print('Movement speed: ', move_speed)
         print('----------')
         
 
 class Motor:
     def __init__(self):
         pass
-        #print('\n---')
-        #self.baud_rate = baud_rate
-        #self.port = port
 
     def send(self, message):
         ser = serial.Serial(port_, baudRate)
         #ser.readline()
-        #ser.readline()
-        #ser.readline()
         print('Executing', message)
         ser.write(message)
-        #print(ser.readline())
-        #print(ser.readline())
-        #print(ser.readline())
         ser.close()
         
+
+class Move(Motor):
+    '''
+    '''
+    def __init__(self, pos1, pos2):
+        Motor.__init__(self)
+        self.pos1 = pos1
+        self.pos2 = pos2
+
+    def run(self):
+        global move_speed
+        route = self.pos1 + self.pos2
+        messageX = '<X, ' + str(move_speed) + ', ' + str(route[0]) + '>'
+        messageY = '<Y, ' + str(move_speed) + ', ' + str(route[1]) + '>'
+        message = bytes(messageX + messageY, 'UTF-8')
+        self.send(message)
+        
+
+class Move_head(Move):
+    '''
+        Cells xy:
+        02, 12, 22, 32
+        01, 11, 21, 31
+        00, 10, 20, 30
+    '''
+    def __init__(self, cellA, cellB):
+        global dx
+        global dy
+        global dx0
+        offset = 0
+        cellA = str(cellA)
+        cellB = str(cellB)
+        if len(cellA) == 1:
+            cellA = '0' + str(cellA)
+        if len(cellB) == 1:
+            cellB = '0' + str(cellB)
+        xA = cellA[0]
+        yA = cellA[1]
+        xB = cellB[0]
+        yB = cellB[1]
+
+        route = np.array([int(xB)-int(xA), int(yB)-int(yA)]) \
+                *np.array([dx,dy])
+        if xA == '0':
+            #offset = dx0
+            route[0] = route[0] + dx0 - dx
+        elif xB == '0':
+            #offset = -dx0
+            route[0] = route[0] - dx0 + dx
+
+        messageX = '<X, ' + str(move_speed) + ', ' + str(route[0]) + '>'
+        messageY = '<Y, ' + str(move_speed) + ', ' + str(route[1]) + '>'
+        self.message = bytes(messageX + messageY, 'UTF-8')
+
+    def run(self):
+        self.send(self.message)
+
+       
+        
+
+
+
 
 class Dispense(Motor):
     '''
@@ -86,25 +148,21 @@ class Rinse(Motor):
         print('\nInitial state', state)
         move_down = b'<ZFLUSH, 100, +60000>'
         move_up = b'<ZFLUSH, 100, -60000>'
-        flush = b'<DCPUMP1, 30, 1500>'
-        first_suc = b'<DCPUMP2, 210, 30000>'
-        second_suc = b'<DCPUMP2, 200, 1000>'
+        flush = b'<DCPUMP4, 80, 5000>'
+        equil_flush = b'<DCPUMP5, 255, 4000>'
+        suc = b'<DCPUMP2, 210, 10000>'
 
         print('Rinsing started')
         self.send(move_down) 
         time.sleep(self.wait_time[0])
         self.send(flush)
         time.sleep(self.wait_time[1])
-        self.send(first_suc)
+        self.send(equil_flush)
         time.sleep(self.wait_time[2])
-        self.send(move_up)
+        self.send(suc)
         time.sleep(self.wait_time[3])
-        self.send(move_down)
-        time.sleep(self.wait_time[4])
-        self.send(second_suc)
-        time.sleep(self.wait_time[5])
         self.send(move_up)
-        time.sleep(self.wait_time[6])
+        time.sleep(self.wait_time[4])
         print('Rinsing finished')
         #global state
         state = 2
